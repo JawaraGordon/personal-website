120bd6bf4c86730a9869ef33f195d64b2269d156
JawaraGordon <jawaragordon@icloud.com>
2023-04-01 20:53:24 -0400

    cleaning up, optimizing and migrating repo

---
+++ main.js
@@ -0,0 +1,42 @@
+window.addEventListener('scroll', function () {
+  if (window.scrollY > 150) {
+    document.querySelector('#navbar').style.opacity = 0.9;
+  } else {
+    document.querySelector('#navbar').style.opacity = 1;
+  }
+});
+
+// Smooth Scrolling
+const navbar = document.getElementById('navbar');
+let scrolled = false;
+
+window.onscroll = function () {
+  if (window.pageYOffset > 100) {
+    navbar.classList.remove('top');
+    if (!scrolled) {
+      navbar.style.transform = 'translateY(-70px)';
+    }
+    setTimeout(function () {
+      navbar.style.transform = 'translateY(0)';
+      scrolled = true;
+    }, 200);
+  } else {
+    navbar.classList.add('top');
+    scrolled = false;
+  }
+};
+
+$('#navbar a').on('click', function (e) {
+  if (this.hash !== '') {
+    e.preventDefault();
+
+    const hash = this.hash;
+
+    $('html, body').animate(
+      {
+        scrollTop: $(hash).offset().top - 100,
+      },
+      800
+    );
+  }
+});
