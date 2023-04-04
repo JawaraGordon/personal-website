24d349d83a38980e659005e1459a178e9f5fbdb8
JawaraGordon <jawaragordon@icloud.com>
2023-04-04 17:46:30 -0400

    fixed bug in modal.js

--- modal.js
+++ modal.js
@@ -1,7 +1,7 @@
-// Get the modal
-// const modal = document.querySelectorAll(
-//   '#id01, #id02, #id03, #id04, #id05, #id06'
-// );
+// // Get the modal
+// // const modal = document.querySelectorAll(
+// //   '#id01, #id02, #id03, #id04, #id05, #id06'
+// // );
 
 const modal1 = document.getElementById('id01');
 const modal2 = document.getElementById('id02');
@@ -9,8 +9,6 @@ const modal3 = document.getElementById('id03');
 const modal4 = document.getElementById('id04');
 const modal5 = document.getElementById('id05');
 const modal6 = document.getElementById('id06');
-
-console.log(typeof modal1);
 
 // When the user clicks anywhere outside of the modal1, close it
 window.onclick = function (event) {
