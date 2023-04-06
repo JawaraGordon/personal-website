5939db1be4bedf1b426e6d2083c9e3796ef12bc3
JawaraGordon <jawaragordon@icloud.com>
2023-04-06 16:40:14 -0400

    added onHover to display project info modals

--- modal.js
+++ modal.js
@@ -3,26 +3,47 @@
 // //   '#id01, #id02, #id03, #id04, #id05, #id06'
 // // );
 
-const modal1 = document.getElementById('id01');
-const modal2 = document.getElementById('id02');
-const modal3 = document.getElementById('id03');
-const modal4 = document.getElementById('id04');
-const modal5 = document.getElementById('id05');
-const modal6 = document.getElementById('id06');
+const modalOne = document.getElementById('id01');
+const modalTwo = document.getElementById('id02');
+const modalThree = document.getElementById('id03');
+const modalFour = document.getElementById('id04');
+const modalFive = document.getElementById('id05');
+const modalSix = document.getElementById('id06');
 
-// When the user clicks anywhere outside of the modal1, close it
+// When the user clicks anywhere outside of the modalOne, close it
 window.onclick = function (event) {
-  if (event.target == modal1) {
-    modal1.style.display = 'none';
-  } else if (event.target == modal2) {
-    modal2.style.display = 'none';
-  } else if (event.target == modal3) {
-    modal3.style.display = 'none';
-  } else if (event.target == modal4) {
-    modal4.style.display = 'none';
-  } else if (event.target == modal5) {
-    modal5.style.display = 'none';
-  } else if (event.target == modal6) {
-    modal6.style.display = 'none';
+  if (event.target == modalOne) {
+    modalOne.style.display = 'none';
+  } else if (event.target == modalTwo) {
+    modalTwo.style.display = 'none';
+  } else if (event.target == modalThree) {
+    modalThree.style.display = 'none';
+  } else if (event.target == modalFour) {
+    modalFour.style.display = 'none';
+  } else if (event.target == modalFive) {
+    modalFive.style.display = 'none';
+  } else if (event.target == modalSix) {
+    modalSix.style.display = 'none';
   }
 };
+
+const onHover = () => {
+  projectOne = document.getElementById('modal1');
+  projectTwo = document.getElementById('modal2');
+  projectThree = document.getElementById('modal3');
+
+  projectOne.addEventListener('mouseover', function () {
+    console.log('onHover');
+    modalOne.style.display = 'block';
+  });
+  projectTwo.addEventListener('mouseover', function () {
+    console.log('onHover');
+    modalTwo.style.display = 'block';
+  });
+  projectThree.addEventListener('mouseover', function () {
+    console.log('onHover');
+    modalFive.style.display = 'block';
+  });
+};
+
+onHover();
